b3901d527a73c858ef98d94e151e0a85d88b32c3
Li <jili1@uiwtx.edu>
2025-04-23 12:18:26 -0500

    first triangle

--- MushroomEngine/src/Framework/Application.cpp
+++ MushroomEngine/src/Framework/Application.cpp
@@ -16,11 +16,30 @@ namespace mr
 
     void Application::Run()
     {
+        float triangleVertexData[]
+        {
+            -0.5, -0.5, 0.0, 0.0, 0.5, 0.0, 0.5, -0.5, 0.0
+        };
+
+        unsigned int vertexArrayObjectId;
+        glGenVertexArrays(1, &vertexArrayObjectId);
+
+        unsigned int vertexBufferObjectId;
+        glGenBuffers(1, &vertexBufferObjectId);
+
+        glBindVertexArray(vertexArrayObjectId);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObjectId);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertexData), triangleVertexData, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); 
+
         while(!glfwWindowShouldClose(mWindow))
         {
             ProcessInput(mWindow);
             glfwPollEvents();		
             glClear(GL_COLOR_BUFFER_BIT);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
             glfwSwapBuffers(mWindow);
         }
     }
